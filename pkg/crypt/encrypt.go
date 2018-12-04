4123495f96185adb08371c2cec08e72c2101c632
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-12-04 12:40:49 +0000

    crypt: implement crypt pkg

---
+++ pkg/crypt/encrypt.go
@@ -0,0 +1,48 @@
+package crypt
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"github.com/pkg/errors"
+	"io"
+)
+
+// Encryptor is used to enecrypt a payload, returning the encrypted payload and key
+type Encryptor interface {
+	Encrypt(payload []byte) (encrypted, key []byte, err error)
+}
+
+// EncryptorFn is any pure function or closure that can be used to enecrypt a
+// payload, returning the encrypted payload and key
+type EncryptorFn func(payload []byte) (encrypted, key []byte, err error)
+
+// Encrypt ensures that EncryptorFn satistfies the Encryptor interface
+func(eFn EncryptorFn) Encrypt(payload []byte)   (encrypted, key []byte, err error) {
+	return eFn(payload)
+}
+
+func Encrypt(payload []byte) (encrypted, key []byte, err error) {
+	k, err := generateNewKey()
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "generating new key for storage")
+	}
+
+	c, err := aes.NewCipher(k)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "creating new aes cipher")
+	}
+
+	var iv [aes.BlockSize]byte
+	stream := cipher.NewOFB(c, iv[:])
+
+	plain := bytes.NewBuffer(payload)
+
+	var eBuff bytes.Buffer
+	writer := &cipher.StreamWriter{S: stream, W: &eBuff}
+	if _, err := io.Copy(writer, plain); err != nil {
+		return nil, nil, errors.Wrap(err, "copying buffer")
+	}
+
+	return eBuff.Bytes(), k, nil
+}
